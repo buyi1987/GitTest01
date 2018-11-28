72d56d23a364d924a115c06e954899009fa547bf
buyi1987 <2845438414@qq.com>
2018-11-28 23:11:59 +0800

    9jian

---
+++ src/com/atguigu/DuDuQiuJian.java
@@ -0,0 +1,24 @@
+package com.atguigu;
+
+/** 
+ * @包名 com.atguigu
+ * @类名 DuDuQiuJian.java
+ * @作者   李道若 
+ * @创建日期 2018年11月28日
+ * @描述 
+ * @版本 V1.0
+ */
+public class DuDuQiuJian {
+
+	/**  
+	 * @方法名: main  
+	 * @功能描述:  
+	 * @param args
+	 * @作者   李道若
+	 * @日期 2018年11月28日
+	 */
+	public static void main(String[] args) {
+		System.out.println("1111111111111111111111");
+	}
+
+}
