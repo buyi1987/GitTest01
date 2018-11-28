1e503aa3d9b2f97ef32a855707353687cbff6db3
buyi1987 <2845438414@qq.com>
2018-11-29 01:37:26 +0800

    update

--- src/com/atguigu/DuDuQiuJian.java
+++ src/com/atguigu/DuDuQiuJian.java
@@ -19,6 +19,8 @@ public class DuDuQiuJian {
 	 */
 	public static void main(String[] args) {
 		System.out.println("1111111111111111111111");
+		System.out.println("222222222222222222222222");
+		System.out.println("2222222222222222222222");
 	}
 
 }
